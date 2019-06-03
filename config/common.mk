7aaa061f88bf7be30933995a3c0d286c2755cbab
ZHANtech™ <hifzhan41@gmail.com>
2019-06-03 08:25:04 +0700

    Add files via upload

---
+++ config/common.mk
@@ -0,0 +1,33 @@
+PRODUCT_BRAND ?= omni
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+# general properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.build.selinux=1 \
+    persist.sys.disable_rescue=true
+
+# Tethering - allow without requiring a provisioning app
+# (for devices that check this)
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
+# enable ADB authentication if not on eng build
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES  += ro.adb.secure=1
+endif
+
+# Additional packages
+-include vendor/batik/config/packages.mk
+
+# Versioning
+-include vendor/batik/config/version.mk
---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,9 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Omni
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/batik/sepolicy/common
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/batik/sepolicy/private
