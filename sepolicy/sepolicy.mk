omnirom/android_vendor_omni

vendor: add exfat and ntfs perms for vold
Change-Id: Ia350d6373f13de4dfab9c2fc6456d8d53de554a9

sepolicy/sepolicy.mk
@@ -7,3 +7,4 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     vendor/omni/sepolicy/common
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/omni/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += vendor/omni/sepolicy/public

BatikRecovery/br_vendor

Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

sepolicy/sepolicy.mk
@@ -7,3 +7,4 @@ BOARD_SEPOLICY_DIRS += \
     vendor/batik/sepolicy/common
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/batik/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += vendor/batik/sepolicy/public