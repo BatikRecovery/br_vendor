b763b46174caf2c9f2193ea49be019d03a979e5c
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2020-04-23 16:36:29 +0700

    Vendor: try auto download magisk

--- config/packages.mk
+++ config/packages.mk
@@ -18,3 +18,7 @@ PRODUCT_PACKAGES += \
     mkshrc_vendor \
     toybox_vendor \
     sh_vendor
+
+# Magisk
+$(shell wget https://github.com/topjohnwu/Magisk/releases/download/v20.4/Magisk-v20.4.zip -O vendor/Batik/tools/magisk.zip)
+$(shell wget https://github.com/topjohnwu/Magisk/releases/download/v20.4/Magisk-uninstaller-20200323.zip -O vendor/Batik/tools/unrootmagisk.zip)
